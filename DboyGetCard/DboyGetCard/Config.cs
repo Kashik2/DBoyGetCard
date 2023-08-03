90fcad22b1b580379db01c6fc62fdc5df47a82d5
Kashik2 <141311655+Kashik2@users.noreply.github.com>
2023-08-03 14:57:52 +0700

    Add files via upload

---
+++ DboyGetCard/DboyGetCard/Config.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DboyGetCard
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ DboyGetCard/DboyGetCard/Plugin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DboyGetCard
+{
+    internal class Plugin
+    {
+    }
+}
